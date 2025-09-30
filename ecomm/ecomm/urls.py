c25cd58921c1c54484c972e105fef8e6c62cce8a
temKann <134260981+n00b3rmn@users.noreply.github.com>
2025-09-30 17:27:56 +0800

    .

--- ecomm/ecomm/urls.py
+++ ecomm/ecomm/urls.py
@@ -1,22 +1,26 @@
-"""
-URL configuration for ecomm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from app1 import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', views.index, name='index'),  
+    path('cart/', views.cart, name='cart'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('order-complete/', views.order_complete, name='order_complete'),
+    path('place-order/', views.place_order, name='place_order'),
+    path('product-detail/', views.product_detail, name='product_detail'),
+    path('register/', views.register, name='register'),
+    path('search-result/', views.search_result, name='search_result'),
+    path('signin/', views.signin, name='signin'),
+    path('store/', views.store, name='store'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / "static")
